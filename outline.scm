3d562bade5c950f37b11ac192a3ec1efb0120878
Richard Feldman <oss@rtfeldman.com>
2024-08-16 15:34:16 -0400

    Add outline.scm

---
+++ outline.scm
@@ -0,0 +1,10 @@
+(file
+  (annotation_type_def
+    (annotation_pre_colon
+    (identifier) @name)) @item)
+
+(file
+  (value_declaration
+    (decl_left
+      (identifier_pattern
+      (identifier) @name))) @item)
